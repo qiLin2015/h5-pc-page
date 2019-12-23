f404bb5bf9f6e8c91e191cad58580b4e6f1bbcd9
zhanglin <zhanglin@noah-fund.com>
2019-12-23 21:57:29 +0800

    first commit

---
+++ src/page/index.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="index_wrap">字体大小</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" rel="stylesheet/scss">
+@media screen and (min-width: 480px) {
+  * {font-size: 12px;}
+  .index_wrap{
+    width: 100%;
+    height: 100vh;
+    background: #ccc;
+    color:red;
+  }
+}
+@media screen and (max-width: 480px) {
+  * {font-size: 12px;}
+  .index_wrap{
+    width: 100%;
+    height: 100vh;
+    background: green;
+    color:red;
+  }
+}
+</style>
