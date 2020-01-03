b6ce37309525450342912e18843455cb7783dca3
zhanglin <zhanglin@noah-fund.com>
2020-01-03 13:40:05 +0800

    add  projectSingle

--- src/page/projectSingle.vue
+++ src/page/projectSingle.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="projectSingleWrap">
     <MenuHeader></MenuHeader>
-    <div class="preNextWrap">
-      <div class="preNext">
-        <div class="pre" @click="handelNav('pre')">PRE<div class="textLine"></div></div>
-        <div class="line">/</div>
-        <div class="next" @click="handelNav('next')">NEXT<div class="textLine"></div></div>
-      </div>
+    <div class="leftWrap">
+      <div class="project">Project <div class="textLine"></div></div>
+      <div class="title">Commercial</div>
+      <div><img class="closeImg" src="@/img/closeLine.png" alt=""></div>
     </div>
-    <div class="productSingleSwiper">
-      <div class="swiper-container" id="productSingleSwiper">
+
+    <div class="singleSwiper">
+      <div class="swiper-container" id="singleSwiper">
         <div class="swiper-wrapper">
           <div class="swiper-slide" v-for="(item, index) in homeSwiperList" :key="index">
             <div class="imgWrap">
@@ -18,9 +17,8 @@
           </div>
         </div>
       </div>
-    </div>
-    <div class="contactWrap" @click="goToContact">
-      <div class="text">contact <div class="textLine"></div></div>
+      <div class="prebtn btn" @click="handelNav('pre')"><i class="el-icon-d-arrow-left"></i></div>
+      <div class="nextBtn btn" @click="handelNav('next')"><i class="el-icon-d-arrow-right"></i></div>
     </div>
   </div>
 </template>
@@ -33,7 +31,7 @@ export default {
   },
     data () {
     return {
-      productSingleSwiper: null,
+      singleSwiper: null,
       homeSwiperList: [{
         imgSrc: require('@/img/project1.png'),
         title: 'Commercial'
@@ -51,43 +49,30 @@ export default {
   },
   methods: {
     render() {
-      this.productSingleSwiper = new Swiper ('#productSingleSwiper', {
+      this.singleSwiper = new Swiper ('#singleSwiper', {
         loop: true, // 循环模式选项
         autoplay: {
-          delay: 3000,//1秒切换一次
+          delay: 3000,
         },
-        effect : 'coverflow',
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        coverflowEffect: {
-          rotate: 50,  // y轴旋转角度
-          stretch: -200, //
-          depth: 100,
-          modifier: 1,
-          slideShadows : false
+        slidesPerView : 3,
+        slidesPerGroup : 1,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
-        on:{
-          init: function(){
-            swiperAnimateCache(this); //隐藏动画元素
-            swiperAnimate(this); //初始化完成开始动画
-          },
-          slideChangeTransitionEnd: function(){
-            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
-          }
-        }
       })
-    },
-    handelNav(type) {
-      if(type === 'next') {
-        this.productSwiper.slideNext();
-      }else {
-        this.productSwiper.slidePrev();
-      }
     },
     goToContact() {
       this.$router.push({
         name: 'Contact'
       })
+    },
+    handelNav(type) {
+      if(type === 'next') {
+        this.singleSwiper.slideNext();
+      }else {
+        this.singleSwiper.slidePrev();
+      }
     }
   },
 }
@@ -99,87 +84,89 @@ export default {
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: space-between;
-  .preNextWrap{
+  justify-content: center;
+  .leftWrap{
+    width: 40%;
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    padding-left: 100px;
-    .preNext{
-      position: relative;
-      font-size: 24px;
-      display: flex;
-      justify-content: space-around;
-      transform: rotate(90deg);
-      .pre, .next{
-        padding:10px 10px;
-        color: #999999;
-        cursor: pointer;
-        position: relative;
-        &:hover{
-          color: #666666;
-          .textLine{
-            width: 55px;
-            height: 2px;
-            background: #898989;;
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            animation: preNextLine 0.8s;
-          }
-        }
-      }
-      .line{
-        padding: 10px 0;
-        color: #999999;
-      }
-    }
-  }
-  .contactWrap{
-    display: flex;
-    align-items: center;
-    padding-right: 100px;
-    .text{
-      position: relative;
-      width: 120px;
-      text-align: center;
+    .project{
       color: #999999;
       font-size: 24px;
       cursor: pointer;
-      transform: rotate(90deg);
+      transform: rotate(-90deg);
+      position: relative;
       &:hover{
         color: #666666;
         .textLine{
-          width: 120px;
+          width: 80px;
           height: 2px;
           background: #666666;
           position: absolute;
           left: 0;
           bottom: 0;
-          animation: contactLine 0.8s;
+          animation: projectLine 0.8s;
         }
       }
     }
+    .title{
+      font-size: 50px;
+    }
+    .closeImg{
+      padding-right: 100px;
+      cursor: pointer;
+    }
   }
-  .productSwiper{
-    width: 60%;
+  .singleSwiper{
+    width: 50%;
     height: 100%;
     box-sizing: border-box;
     display: flex;
     align-items: center;
+    position: relative;
+    .btn{
+      position: absolute;
+      top: 50%;
+      margin-top: -20px;
+      width: 40px;
+      height: 40px;
+      border: 4px solid #EEEEEE;
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      z-index: 100;
+      box-sizing: border-box;
+      color: #EEEEEE;
+      font-size: 24px;
+      font-weight: 900;
+      &:hover{
+        border: 4px solid #3ab1df;
+        color: #3ab1df
+      }
+    }
+    .prebtn{
+      left: -40px;
+    }
+    .nextBtn{
+      right: -40px;
+    }
     .swiper-container {
       width: 100%;
       .swiper-wrapper{
         .swiper-slide{
-          width: 200px;
+          width:100%;
+          padding: 0 20px;
+          box-sizing: border-box;
+          cursor: pointer;
           .slideTitle{
             text-align: center;
             font-size: 65px;
             letter-spacing: 3px;
           }
           .imgWrap{
-            width: 320px;
             img{
-              width: 320px;
+              width: 100%;
               height: auto;
             }
           }
@@ -187,15 +174,70 @@ export default {
       }
     }
   }
+  // .preNextWrap{
+  //   display: flex;
+  //   align-items: center;
+  //   padding-left: 100px;
+  //   .preNext{
+  //     position: relative;
+  //     font-size: 24px;
+  //     display: flex;
+  //     justify-content: space-around;
+  //     transform: rotate(90deg);
+  //     .pre, .next{
+  //       padding:10px 10px;
+  //       color: #999999;
+  //       cursor: pointer;
+  //       position: relative;
+  //       &:hover{
+  //         color: #666666;
+  //         .textLine{
+  //           width: 55px;
+  //           height: 2px;
+  //           background: #898989;;
+  //           position: absolute;
+  //           left: 0;
+  //           bottom: 0;
+  //           animation: preNextLine 0.8s;
+  //         }
+  //       }
+  //     }
+  //     .line{
+  //       padding: 10px 0;
+  //       color: #999999;
+  //     }
+  //   }
+  // }
+  // .contactWrap{
+  //   display: flex;
+  //   align-items: center;
+  //   padding-right: 100px;
+  //   .text{
+  //     position: relative;
+  //     width: 120px;
+  //     text-align: center;
+  //     color: #999999;
+  //     font-size: 24px;
+  //     cursor: pointer;
+  //     transform: rotate(90deg);
+  //     &:hover{
+  //       color: #666666;
+  //       .textLine{
+  //         width: 120px;
+  //         height: 2px;
+  //         background: #666666;
+  //         position: absolute;
+  //         left: 0;
+  //         bottom: 0;
+  //         animation: contactLine 0.8s;
+  //       }
+  //     }
+  //   }
+  // }
 }
-@keyframes contactLine
+@keyframes projectLine
 {
   from {width: 0px;}
-  to {width: 120px;}
-}
-@keyframes preNextLine
-{
-  from {width: 0px;}
-  to {width: 55px;}
+  to {width: 80px;}
 }
 </style>
